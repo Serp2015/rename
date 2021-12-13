bb1297794ebb1f8fafb27be78d524921b762a450
Serp2015 <serpych@yandex.ru>
2021-12-13 22:20:37 +0400

    v2

--- src/CopyFiles.java
+++ src/CopyFiles.java
@@ -1,26 +1,43 @@
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class CopyFiles {
 
     public void copy(Map<String, String> map) {
+        Set<String> absent = new HashSet<>();
         File fileP1310 = new File("files/P1310");
         File[] listP1310Files = fileP1310.listFiles();
         if (listP1310Files != null) {
-            for (File p1310Files : listP1310Files) {
-                String fileName = p1310Files.getName();
+            for (File p1310File : listP1310Files) {
+                String fileName = p1310File.getName();
                 for (Map.Entry<String, String> entry : map.entrySet()) {
-                    if (fileName.contains(entry.getKey())) {
-                        File newFile = new File("files/P1340/" + entry.getValue() + ".txt");
+                    if (fileName.replaceAll("-", "")
+                            .contains(entry.getKey().replaceAll("-", ""))
+                            && fileName.contains("-" + entry.getKey().substring(0, 3))) {
+                        File newFile = new File("files/P1340/" + entry.getValue());
                         try {
-                            Files.copy(p1310Files.toPath(), newFile.toPath());
+                            Files.copy(p1310File.toPath(), newFile.toPath());
+                            absent.add(entry.getValue());
                         } catch (IOException e) {
-                            e.printStackTrace();
+                            System.out.println(fileName + "   " + newFile + " ошибка записи");
                         }
                     }
                 }
+            }
+        }
+        int i = 0;
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            boolean isPresent = false;
+            for (String s : absent) {
+                if (entry.getValue().equals(s)) isPresent = true;
+            }
+            if (!isPresent) {
+                i++;
+                System.out.println(i + " " + entry.getValue() + " отсутствует");
             }
         }
     }
